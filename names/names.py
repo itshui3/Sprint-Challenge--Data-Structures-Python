86e943e2d51c85009da7ce797b2dae069a5b29b6
Hui <jeffrey.x.hui@gmail.com>
2020-06-25 15:12:37 -0700

    names with bst

--- names/names.py
+++ names/names.py
@@ -14,12 +14,82 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-names = set()
-for name_1 in names_1:
-    names.add(name_1)
-for name_2 in names_2:
-    if name_2 in names:
-        duplicates.append(name_2)
+names = []
+# for name_1 in names_1:
+#     names.append(name_1)
+
+# names_1.sort()
+
+# def re_check_dupes(val, search_arr):
+
+#     center = len(search_arr) - 1 // 2
+#     if val == search_arr[center]: return True
+
+#     left = search_arr[:center]
+#     right = search_arr[center + 1:]
+
+#     if val > center:
+#         return re_check_dupes(val, right)
+
+#     elif val < center:
+#         return re_check_dupes(val, left)
+
+#     return False
+
+# for i in names_2:
+#     if re_check_dupes(i, names_1):
+#         names.append(i)
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+
+        if value < self.value:
+
+            if self.left is None:
+                self.left = BST(value)
+                return
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+                return
+            else:
+                self.right.insert(value)
+
+    def contains(self, value):
+
+        if self.value == value: return True
+
+        elif value < self.value:
+
+            if self.left is None: return False
+
+            else:
+                return self.left.contains(value)
+
+        else:
+
+            if self.right is None: return False
+
+            else:
+                return self.right.contains(value)
+
+bst = BST(names_1[0])
+if bst.value > names_1[1]:
+    print('true')
+
+for i in range(1, len(names_1)):
+    bst.insert(names_1[i])
+
+for i in names_2:
+    if bst.contains(i):
+        duplicates.append(i)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
